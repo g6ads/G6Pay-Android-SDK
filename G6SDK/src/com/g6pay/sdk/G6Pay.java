5b1f59ff265b1bb5c6b4cccaac5bca5cbc9da2b7
Peter Hsu <silversc3@yahoo.com>
2011-04-13 17:39:51 -0700

    Added distribution packaging script and some basic versioning

--- G6SDK/src/com/g6pay/sdk/G6Pay.java
+++ G6SDK/src/com/g6pay/sdk/G6Pay.java
@@ -67,6 +67,9 @@ import android.util.Log;
  *
  */
 public class G6Pay implements G6AdvertiserIF, G6PublisherIF {
+    
+    public static final String VERSION = "1.0";
+    
     private static final String mLogStr = "G6Pay";
 
     private static G6Pay _theInstance = null;
